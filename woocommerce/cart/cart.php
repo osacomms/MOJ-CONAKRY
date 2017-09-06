d5961cb85e72646233f3590dfde4b96790207684
osacomms <30481880+osacomms@users.noreply.github.com>
2017-09-06 20:12:49 +0100

    Add files via upload

---
+++ woocommerce/cart/cart.php
@@ -0,0 +1,1 @@
+<?php/** * Cart Page * * @version     3.0.3 */if ( ! defined( 'ABSPATH' ) ) {    exit;}wc_print_notices();do_action( 'woocommerce_before_cart' );get_template_part( "woocommerce/cart/cart", porto_cart_version() );do_action( 'woocommerce_after_cart' );
